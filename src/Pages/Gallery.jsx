d64a658bbc4bb9e17ba1fd419369e1bfccfc8927
olayode demola <demolaolayode6@gmail.com>
2024-05-25 17:00:35 -0700

    added more videos to the gallery page

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -3,9 +3,10 @@ import '../Styles/Gallery.css'
 import NissanVid from '../VideoPlayer/NissanVid';
 const Gallery = () => {
   return (
-    <div>
+    <div className='gallery-container m-auto'>
       <section>
-        Videos
+        <h6 className='mb-3 mt-3'>Check Out Some Of Our Beautiful Works</h6>
+        
         <NissanVid/>
       </section>
     </div>
--- src/VideoPlayer/NissanVid.jsx
+++ src/VideoPlayer/NissanVid.jsx
@@ -2,26 +2,84 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 // import { DefaultPlayer as Video } from 'react-html5video';
 // import 'react-html5video/dist/styles.css';
-import NissanVidd from '../Videos/nissan-vidd.mp4';
+import NissanVideo from '../Videos/Nisssan-video.mp4'
+import MaybachVid from '../Videos/maybach-vid.mp4';
+import ToyotaVid from '../Videos/toyotahighlander-vidd.mp4'
+import ToyotaJeepVid from '../Videos/Toyota-jeep.mp4'
 import '../Styles/NissanVid.css'
 
 const NissanVid = () => {
   return (
-    <div className='m-auto  d-flex justify-content-center nissan-vid-container'>
+    <div className='m-auto w-100'>
 
+      <section>
+        <ul>
+        <li>Finished spay work on a Nisssan Car</li>
+        </ul>
+      <div className='m-auto mb-4 d-flex justify-content-center nissan-vid-container '>
       <ReactPlayer 
-      url={NissanVidd}
+      url={NissanVideo}
+      playing={true}
+      controls={true}
+      
+      width='302px'
+      height='310px'
+      className=''
+      />
+      </div>
+      </section>
+
+      <section>
+      <ul>
+        <li>Finished spay work on a Mercedes-Maybach</li>
+        </ul>
+      <div className='m-auto mb-4 d-flex justify-content-center nissan-vid-container'>
+      <ReactPlayer 
+      url={MaybachVid}
       playing={true}
       controls={true}
       muted
-      width='320px'
-      height='330px'
-      className=''
-      
-      
+      width='300px'
+      height='365px'
       
       />
-      
+      </div>
+      </section>
+
+      <section>
+      <ul>
+        <li>Finished spay work on a Toyota Highlander</li>
+        </ul>
+      <div className='m-auto mb-4 d-flex justify-content-center nissan-vid-container'>
+      <ReactPlayer 
+      url={ToyotaVid}
+      playing={true}
+      controls={true}
+      muted
+      width='295px'
+      height='365px'
+      className=''
+      />
+      </div>
+      </section>
+
+      <section>
+      <ul>
+        <li>Finished spay work on a Toyota Jeep</li>
+        </ul>
+      <div className='m-auto mb-4 d-flex justify-content-center nissan-vid-container'>
+      <ReactPlayer 
+      url={ToyotaJeepVid}
+      playing={true}
+      controls={true}
+      muted
+      width='295px'
+      height='365px'
+      className=''
+      />
+      </div>
+      </section>
+
     </div>
 
    
